103e2b508de2cabedb4f5555c9c55fedc3d0c10a
Silvio Paganini <s2paganini@gmail.com>
2018-05-03 17:54:55 +0100

    separates common chunk, hashes compiled JS files and generates index.html from template

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
+const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const moment = require('moment');
 
 module.exports = {
     entry: './src/scripts/main.js',
     mode: process.env.NODE_ENV || 'development',
     watch: process.env.NODE_ENV !== 'production',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'scripts/main.js'
-    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'common',
+                    chunks: 'all'
+                }
+            }
+        }
     },
     module: {
         rules: [
@@ -27,25 +37,25 @@ module.exports = {
                     loader: 'babel-loader',
                 }
             },
-            // {
-            //     test: /\.(glsl|frag|vert)$/,
-            //     exclude: /node_modules/,
-            //     use: {
-            //         loader: 'glslify-import-loader'
-            //     }
-            // },
-            // {
-            //     test: /\.(glsl|frag|vert)$/,
-            //     exclude: /node_modules/,
-            //     use: {
-            //         loader: 'raw-loader'
-            //     }
-            // },
-
         ]
     },
     stats: {
         colors: true
     },
+    output: {
+        filename: 'scripts/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/template/index.html',
+            hash: true,
+        }),
+        new webpack.DefinePlugin({
+            'process.env' : {
+                VERSION: JSON.stringify(`${require('./package.json').name}\nVersion: ${require('./package.json').version}\nBuild: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`),
+            }
+        })
+    ],
     devtool: 'source-map'
 };
