b2e089da301b335d87e2007e9d2358079c7e0d84
brunoimbrizi <bruno.imbrizi@gmail.com>
2020-01-10 09:57:24 +0000

    Update build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const moment = require('moment');
 
@@ -57,6 +58,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+        new CleanWebpackPlugin(
+            ['dist/scripts']
+        ),
         new HtmlWebpackPlugin({
             template: 'src/template/viz.html',
             filename: 'viz.html'
